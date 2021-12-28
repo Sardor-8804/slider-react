f2e5d06ec2f8232b3ceff813031d1268935d3fc0
Ergashev <sergashev8804@gmail.com>
2021-12-28 20:55:32 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const people = [
+  {
+    id: 1,
+    image:'./images/2.jpeg',
+    name: 'Migel ferguson',
+    title: 'office manager',
+    quote:
+      'As we look ahead into the next century, leaders will be those who empower others',
+  },
+  {
+    id: 2,
+    image:
+      './images/3.jpeg',
+    name: 'Kristina',
+    title: 'Developer',
+    quote:
+      'We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins.',
+  },
+  {
+    id: 3,
+    image:
+      './images/4.jpeg',
+    name: 'peter smith',
+    title: 'Product designer',
+    quote:
+      'When I am working on a problem, I never think about beauty but when I have finished, if the solution is not beautiful, I know it is wrong',
+  },
+  {
+    id: 4,
+    image:
+      './images/5.jpeg',
+    name: 'Peter andersen',
+    title: 'The boss',
+    quote:
+      'If your actions inspire others to dream more, learn more, do more and become more, you are a leader.',
+  },
+];
+
+export default people;
